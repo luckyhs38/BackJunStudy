9ca234e565269d166a0b6ecba58d068029a80b9e
Lee HyunSeung <luckyhs38@daum.net>
2023-08-08 01:02:47 +0900

    20230807 입출력 문제

---
+++ study/src/one/Test10950.java
@@ -0,0 +1,28 @@
+package one;
+
+import java.util.Scanner;
+
+/***************************************************
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+각 테스트 케이스마다 A+B를 출력한다.
+***************************************************/
+
+public class Test10950 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int I = sc.nextInt();
+		for(int i=0; i<I; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+		}
+		
+
+	}
+
+}
---
+++ study/src/one/Test2558.java
@@ -0,0 +1,18 @@
+package one;
+
+import java.util.Scanner;
+
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+public class Test2558 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A , B ;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A+B);
+
+	}
+
+}
