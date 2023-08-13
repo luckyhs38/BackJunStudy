13bf68e71ec7a7ce6ead4b19762f0e849442e73e
Lee HyunSeung <62979122+luckyhs38@users.noreply.github.com>
2023-08-13 22:54:59 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 576 ms, Memory: 23468 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<(2*n-1)-(i*2);j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		
+		for (int i = 0; i < n - 1; i++) {	// N+1 ~ 2N-1
+	        
+			for (int j = 1; j < (n - 1) - i; j++) {	
+				System.out.print(" ");
+			}
+ 
+			for (int k = 0; k < 3 + 2 * i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
